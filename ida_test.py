4a508d20d566cf12c53ad144fa9d9edad35d5d1f
idabh <idabanghansen@gmail.com>
2022-05-05 19:15:06 +0200

    playing with clip

---
+++ ida_test.py
@@ -0,0 +1,23 @@
+# pip install ftfy regex tqdm
+# pip install git+https://github.com/openai/CLIP.git
+
+# --- RUN CLIP ---
+
+import torch
+import clip
+from PIL import Image
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+image = preprocess(Image.open("strawberry.jpg")).unsqueeze(0).to(device)
+text = clip.tokenize(["a red strawberry", "a strawberry"]).to(device)
+
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
