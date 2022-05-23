2c6fd788e1ee8b46bbac49bfe49f6476fa631a2e
idabh <idabanghansen@gmail.com>
2022-05-23 16:19:58 +0200

    mT5 changes + R plot script

--- prepare_data_splits.py
+++ prepare_data_splits.py
@@ -9,20 +9,20 @@ from datasets import Dataset
 from sklearn.model_selection import train_test_split
 from datasets import load_dataset
 
-# %% MAKE 50K SUBSET FROM CLEANED DATA
-ds_clean = load_dataset('csv', data_files='/work/69831/tok_ds_clean.csv')
-df_clean = pd.DataFrame(ds) # pandas dataframe version
-df50k_clean = df_clean[:50000] # make 50k clean subset
-ds50k_clean = Dataset.from_pandas(df50k_clean) # make dataset format
+# %% MAKE 25K SUBSET FROM CLEANED DATA
+ds_clean = load_dataset('csv', data_files='C:/Users/idaba/OneDrive/Dokumenter/COGNITIVE_SCIENCE/data_science/tok_ds_clean.csv')
+df_clean = pd.DataFrame(ds_clean['train']) # pandas dataframe version
+df25k_clean = df_clean[:25000] # make 50k clean subset
+ds25k_clean = Dataset.from_pandas(df25k_clean) # make dataset format
 
-# %% make 50k clean train test val splits
-train50k_clean, test50k_clean = ds50k_clean.train_test_split(test_size=5000).values() # 5000 = absolute size specified (out of 50k)
-train50k_clean, val50k_clean = train50k_clean.train_test_split(test_size=5000).values()
+# %% make 25k clean train test val splits
+train25k_clean, test25k_clean = ds25k_clean.train_test_split(test_size=2500).values() # 2500 = absolute size specified (out of 25k)
+train25k_clean, val25k_clean = train25k_clean.train_test_split(test_size=2500).values()
 
-# %% save train test and val CSV for 50k clean
-train50k_clean.to_csv("data/train50k_clean.csv")
-test50k_clean.to_csv("data/test50k_clean.csv")
-val50k_clean.to_csv("data/val50k_clean.csv")
+# %% save train test and val CSV for 25k clean
+train25k_clean.to_csv("train25k_clean.csv")
+test25k_clean.to_csv("test25k_clean.csv")
+val25k_clean.to_csv("val25k_clean.csv")
 
 # %% make splits for ALL clean
 len(ds_clean) # test
@@ -35,6 +35,18 @@ train_clean, val_clean = train_clean.train_test_split(test_size=test_len).values
 train_clean.to_csv("data/train_clean.csv")
 test_clean.to_csv("data/test_clean.csv")
 val_clean.to_csv("data/val_clean.csv")
+
+
+# %% CHECK IT!
+train25k = Dataset.from_pandas(pd.read_csv("train25k_clean.csv", usecols=['text','summary'])) # training data
+
+# %%
+train1k = Dataset.from_pandas(pd.read_csv("train1k.csv", usecols=['text','summary'])) # training data
+
+# %%
+train25test = train25k['train']
+
+
 
 # --- @IDA: RUN UNTIL HERE, SAVE FILES, DOUBLE CHECK SIZE
 
