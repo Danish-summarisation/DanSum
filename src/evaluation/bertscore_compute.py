e833794529ced32e7c06b04dbb21bdd5b4928cf3
KennethEnevoldsen <kennethcenevolsen@gmail.com>
2022-07-04 15:37:55 +0200

    removed unused imports and formatted to black

--- src/evaluation/bertscore_compute.py
+++ src/evaluation/bertscore_compute.py
@@ -1,19 +1,20 @@
 # %% import modules
-from datasets import load_metric
-import numpy as np
 import pickle
+
+import numpy as np
+from datasets import load_metric
 
 # %% load bertscore
 metric = load_metric("bertscore")
 
 # %% load data
-preds_results = np.load('/results/daT-base-summariser_preds.npy', allow_pickle = True)
-preds = [d['pred'] for d in preds_results]
-labels = [d['summary'] for d in preds_results]
+preds_results = np.load("/results/daT-base-summariser_preds.npy", allow_pickle=True)
+preds = [d["pred"] for d in preds_results]
+labels = [d["summary"] for d in preds_results]
 
 # %% compute bertscores
 bertscores = metric.compute(predictions=preds, references=labels, lang="da")
 
 # %% save results
-with open('results/daT5-base-summariser_bertscore', 'wb') as fp:
+with open("results/daT5-base-summariser_bertscore", "wb") as fp:
     pickle.dump(bertscores, fp)
--- src/preprocess/prepare_data_splits.py
+++ src/preprocess/prepare_data_splits.py
@@ -1,30 +1,31 @@
-'''
+"""
 This script prepares subsets and train-test-val splits of the data.
-'''
+"""
 
 # %% LOAD PACKAGES
 import pandas as pd
-import datasets
-from datasets import Dataset
-from sklearn.model_selection import train_test_split
-from datasets import load_dataset
+from datasets import Dataset, load_dataset
 
 # %% LOAD DATA
-ds_clean = load_dataset('csv', data_files='/work/69831/data/tok_ds_clean.csv')
-ds_clean = ds_clean['train'] # right format
-df_clean = pd.DataFrame(ds_clean) # pandas dataframe version
+ds_clean = load_dataset("csv", data_files="/work/69831/data/tok_ds_clean.csv")
+ds_clean = ds_clean["train"]  # right format
+df_clean = pd.DataFrame(ds_clean)  # pandas dataframe version
 
 # %% --- MAKE 25K CLEAN 80-10-10 SUBSET SPLIT
-df25k_clean = df_clean[:25000] # make 25k clean subset
-ds25k_clean = Dataset.from_pandas(df25k_clean) # make dataset format
-df25k_clean.to_csv("clean25k.csv") # save csv
+df25k_clean = df_clean[:25000]  # make 25k clean subset
+ds25k_clean = Dataset.from_pandas(df25k_clean)  # make dataset format
+df25k_clean.to_csv("clean25k.csv")  # save csv
 
 # %% create splits
-test_len = round(len(ds25k_clean) / 10) # test is 10%
-val_len = round(len(ds25k_clean) / 10) # validation is 10%
+test_len = round(len(ds25k_clean) / 10)  # test is 10%
+val_len = round(len(ds25k_clean) / 10)  # validation is 10%
 
-train25k_clean, test25k_clean = ds25k_clean.train_test_split(test_size=test_len).values() # absolute size specified
-train25k_clean, val25k_clean = train25k_clean.train_test_split(test_size=val_len).values()
+train25k_clean, test25k_clean = ds25k_clean.train_test_split(
+    test_size=test_len
+).values()  # absolute size specified
+train25k_clean, val25k_clean = train25k_clean.train_test_split(
+    test_size=val_len
+).values()
 
 # %% save train test and val CSV
 train25k_clean.to_csv("train25k_clean.csv")
@@ -32,9 +33,11 @@ test25k_clean.to_csv("test25k_clean.csv")
 val25k_clean.to_csv("val25k_clean.csv")
 
 # %% --- MAKE ALL CLEAN 80-10-1 SUBSET SPLIT
-test_len = round(len(ds_clean) / 10) # test is 10%
-val_len = round(len(ds_clean) / 100) # validation is 1%
-train_clean1, test_clean1 = ds_clean.train_test_split(test_size=test_len).values() # absolute size specified
+test_len = round(len(ds_clean) / 10)  # test is 10%
+val_len = round(len(ds_clean) / 100)  # validation is 1%
+train_clean1, test_clean1 = ds_clean.train_test_split(
+    test_size=test_len
+).values()  # absolute size specified
 train_clean1, val_clean1 = train_clean1.train_test_split(test_size=val_len).values()
 
 # %% save train test and val CSV for all clean
