a85f8d7feb2de9f791915388b43605f50958b48e
idabh <idabanghansen@gmail.com>
2022-07-05 13:56:14 +0200

    draft for data cleaning

---
+++ src/preprocess/clean_data.py
@@ -0,0 +1,20 @@
+'''
+This script preprocesses the data from the DaNews and DaNewsroom data sets.
+'''
+
+import pandas as pd
+from datasets import Dataset, load_dataset
+
+# %% Combine all data into same format?
+
+
+# %% Remove duplicates (and near duplicates)
+
+
+# %% Remove extremely short (or long) articles
+
+
+# %% Remove entries based on other criteria?
+
+
+# %% SUBSET based on density
