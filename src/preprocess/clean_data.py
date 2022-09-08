90a50bb6c76491370fb91318d86c0242de054f18
anja220 <katrinesofiemn@hotmail.dk>
2022-09-08 18:09:25 +0200

    removing redundant text

--- src/preprocess/clean_data.py
+++ src/preprocess/clean_data.py
@@ -2,7 +2,6 @@
 This script preprocesses the data from the DaNewsroom data sets.
 Applying cutoffs, filtering out the abstractive data and saving the data files.
 '''
-# OBS currently only with a 100k subset due to memory failing on my local laptop. But should work on all by removing small part of line 36
 # %% Load modules
 import pandas as pd
 from datasets import Dataset, load_dataset
@@ -15,7 +14,7 @@ import matplotlib.pyplot as plt
 
 def preprocess_function(examples):
     # concatenate prefix and article into one input
-    inputs = [prefix + doc for doc in examples["text"]]
+    inputs = [doc for doc in examples["text"]]
 
     # tokenize the input + truncate to max input length
     model_inputs = tokenizer(inputs, truncation=True) 
@@ -30,31 +29,27 @@ def preprocess_function(examples):
     return model_inputs
 
 # %% Load raw data
-ds = load_dataset("csv", data_files="../../../danewsroom.csv")
-ds_abstractive = ds["train"]
+ds = load_dataset("csv", data_files="../../../danewsroom.csv") # change path
+ds_train = ds["train"]
 
 # %% Filter by density
-df_abstractive = pd.DataFrame(ds_abstractive)
+df_abstractive = pd.DataFrame(ds_train)
 df_abstractive = df_abstractive[df_abstractive.density <= 1.5] 
 print(len(df_abstractive)) # 287205
-df_abstractive.to_csv("data/df_abstractive.csv")  # save csv
-# Keep a raw subset of 25k? ?? But how when stuff is removed
-# maybe not needed since we will only train on the cleaned data right?
+df_abstractive.to_csv("df_abstractive.csv")  # save csv
 
 # %% Clean by filter token article/summary length cutoffs
-df_abstractive = pd.read_csv('data/df_abstractive.csv')
+df_abstractive = pd.read_csv('df_abstractive.csv') # load csv
 
 # %% Convert data format
-df1k = df_abstractive[:25000] # remove subset when finished
-df1k_ds = Dataset.from_pandas(df1k)
-df1k_dd = datasets.DatasetDict({"data":df1k_ds})
+ds_abstractive = Dataset.from_pandas(df_abstractive)
+dd_abstractive = datasets.DatasetDict({"data":ds_abstractive})
 
 # %% Load tokenizer
 tokenizer = T5TokenizerFast.from_pretrained("google/mt5-small") # choose tokenizer
-prefix = "summarize: " # prefix
 
 # %% tokenize chosen data
-tok_dd = df1k_dd.map(preprocess_function, batched=True) 
+tok_dd = dd_abstractive.map(preprocess_function, batched=True) 
 tok_ds = tok_dd ['data'] # turn back into dataset (not dict)
 
 # %% add features to ds with the TOKENISED lengths of ref summary and text/article
@@ -62,16 +57,6 @@ tok_text_len = [len(text) for text in tok_ds['input_ids']] # input ids is the ar
 tok_ds = tok_ds.add_column("tok_text_len", tok_text_len)
 tok_sum_len = [len(summary) for summary in tok_ds['labels']] # labels is the ref summaries
 tok_ds = tok_ds.add_column("tok_sum_len", tok_sum_len)
-
-# %% check the stats
-print("min sum:")
-print(min((tok_ds)['tok_sum_len']))
-print("max sum:")
-print(max((tok_ds)['tok_sum_len']))
-print("min text:")
-print(min((tok_ds)['tok_text_len']))
-print("max text: ")
-print(max((tok_ds)['tok_text_len']))
 
 # %% Make plot with summary length and text length to define cutoffs
 # histogram of summary token length
@@ -100,46 +85,6 @@ graph.text(30000, 700, f'Upper cutoff: {round(max_text_len, 1)}')
 plt.savefig('text_tokens_hist.png')
 plt.show()
 
-
-# %% Check the edge cases qualitatively
-tok_df = pd.DataFrame(tok_ds) 
-# shortest summaries
-index = tok_df.loc[tok_df['tok_sum_len'] == 9]
-# The first here is a mistake and not a summary 
-index = index.loc[tok_df['Unnamed: 0'] != 109]
-index['summary'][10:30]
-index_ds = Dataset.from_pandas(index)
-print(index_ds['summary'][0:10])
-
-#%% shorter than 9
-tok_df = pd.DataFrame(tok_ds) 
-index = tok_df.loc[tok_df['tok_sum_len'] <= 8]
-index_ds = Dataset.from_pandas(index)
-print(index_ds['summary'][0:10])
-
-#%% Longest summaries
-tok_df = pd.DataFrame(tok_ds) 
-index = tok_df.loc[tok_df['tok_sum_len'] == 80]
-index_ds = Dataset.from_pandas(index)
-
-#%% Longer than 100 summaries
-tok_df = pd.DataFrame(tok_ds) 
-index = tok_df.loc[tok_df['tok_sum_len'] == 100]
-index_ds = Dataset.from_pandas(index)
-print(index_ds['summary'][0:10])
-
-#%% Texts short 91
-tok_df = pd.DataFrame(tok_ds) 
-index = tok_df.loc[tok_df['tok_text_len'] == 91]
-index_ds = Dataset.from_pandas(index)
-print(index_ds['text'][0:10])
-
-#%% Texts long 2119
-tok_df = pd.DataFrame(tok_ds) 
-index = tok_df.loc[tok_df['tok_text_len'] >= 2119]
-index_ds = Dataset.from_pandas(index)
-print(index_ds['text'][0:10])
-
 # %% Filtering based on cutoffs
 tok_df = pd.DataFrame(tok_ds) # pandas dataframe version
 tok_df_clean = tok_df
@@ -153,7 +98,6 @@ tok_df_clean = tok_df_clean.loc[tok_df_clean['tok_text_len'] >= min_text_len]
 tok_df_clean = tok_df_clean.loc[tok_df_clean['tok_text_len'] <= max_text_len]
 # make dataset format
 tok_ds_clean = Dataset.from_pandas(tok_df_clean)
-# length should be 230012 after cleaning (with 15, 128, 150, 1500)
 
 # %% Plot the newly cut data summary lengths
 graph = seaborn.histplot(tok_df_clean['tok_sum_len'], color = "green")
@@ -170,23 +114,5 @@ plt.savefig('text_tokens_cleaned.png')
 plt.show()
 
 # %% save it
-tok_ds_clean.to_csv('datatok_ds_clean.csv')
-# Went from 25k to 23425
-# Now only 20837 with 90% of data cutoffs
-# 96% return 23172
-
-#### Not used
-#### Pipeline for other dataset e.g. Danews
-# %% Combine all data into same format?
-
-
-# %% Remove duplicates (and near duplicates)
-
-
-# %% Remove extremely short (or long) articles
-
-
-# %% Remove entries based on other criteria?
-
-
-# %% SUBSET based on density
+tok_ds_clean.to_csv('tok_ds_clean.csv')
+# %%
--- src/preprocess/prepare_data_splits.py
+++ src/preprocess/prepare_data_splits.py
@@ -8,14 +8,14 @@ from datasets import Dataset, load_dataset
 seed = 22 # implement a seed to ensure replication
 
 # %% LOAD DATA
-ds_clean = load_dataset("csv", data_files="data/tok_ds_clean.csv") #/work/69831/data/
+ds_clean = load_dataset("csv", data_files="tok_ds_clean.csv")
 ds_clean = ds_clean["train"]  # right format
 df_clean = pd.DataFrame(ds_clean)  # pandas dataframe version
 
 # %% --- MAKE 25K CLEAN 80-10-10 SUBSET SPLIT
 df25k_clean = df_clean[:25000]  # make 25k clean subset
 ds25k_clean = Dataset.from_pandas(df25k_clean)  # make dataset format
-df25k_clean.to_csv("data/clean25k.csv")  # save csv
+df25k_clean.to_csv("clean25k.csv")  # save csv
 
 # %% create splits
 test_len = round(len(ds25k_clean) / 10)  # test is 10%
@@ -29,9 +29,9 @@ train25k_clean, val25k_clean = train25k_clean.train_test_split(
 ).values()
 
 # %% save train test and val CSV
-train25k_clean.to_csv("data/train25k_clean.csv")
-test25k_clean.to_csv("data/test25k_clean.csv")
-val25k_clean.to_csv("data/val25k_clean.csv")
+train25k_clean.to_csv("train25k_clean.csv")
+test25k_clean.to_csv("test25k_clean.csv")
+val25k_clean.to_csv("val25k_clean.csv")
 
 # %% --- MAKE ALL CLEAN 89-10-1 SUBSET SPLIT
 test_len = round(len(ds_clean) / 10)  # test is 10%
@@ -44,6 +44,6 @@ train_clean1, val_clean1 = train_clean1.train_test_split(
     ).values()
 
 # %% save train test and val CSV for all clean
-train_clean1.to_csv("data/train_clean1.csv")
-test_clean1.to_csv("data/test_clean1.csv")
-val_clean1.to_csv("data/val_clean1.csv")
+train_clean1.to_csv("train_clean1.csv")
+test_clean1.to_csv("test_clean1.csv")
+val_clean1.to_csv("val_clean1.csv")
