685bea64d1160bd3c38469716c1beaf0677a2d7d
sarakolding <sarakolding@live.dk>
2022-10-07 22:32:38 +0200

    added packages for quality filters and changed paths

--- src/evaluation/compute_density.py
+++ src/evaluation/compute_density.py
@@ -1,17 +1,19 @@
 # %% import modules
 import numpy as np
 import pandas as pd
-from newsroom.analyze import Fragments
+from fragments import Fragments
 
 # %% load data
-preds_results = np.load("/results/daT5-base-summariser_preds.npy", allow_pickle=True)
+preds_results = np.load("/home/sarakolind/DanSum/results/daT5-base-summariser_preds.npy", allow_pickle=True)
 preds = [d["pred"] for d in preds_results]
 texts = [d["text"] for d in preds_results]
 
 # %% calculate density
 fragments = [Fragments(n[0], n[1], lang="da") for n in zip(preds, texts)]
+print(type(fragments))
 densities = [n.density() for n in fragments]
-
+print(type(densities))
+print(np.mean(densities))
 # %% save results
-d_frame = pd.DataFrame(densities)
-d_frame.to_csv("daT5-base-summariser_density.csv")
+#d_frame = pd.DataFrame(densities)
+#d_frame.to_csv("daT5-base-summariser_density.csv")
