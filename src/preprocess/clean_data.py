7b7e779b9241023014ef00b048a10bc86b2a83cb
Sara Kolding <74907693+sarakolding@users.noreply.github.com>
2023-04-27 16:02:28 +0200

    clean up preprocessing

--- src/preprocess/clean_data.py
+++ src/preprocess/clean_data.py
@@ -1,14 +1,16 @@
 """
 This script preprocesses the data from the DaNewsroom data sets.
 Applying cutoffs, filtering out the abstractive data and saving the data files.
+
+Quality filter is from the following github: https://github.com/centre-for-humanities-computing/danish-foundation-models/blob/b16765c065818f9d4b162d2b2ab9b3dae7d252ea/src/dfm/cleaning/quality_filter.py
 """
-# %% Load modules
 import datasets
 import numpy as np
 import pandas as pd
 from datasets import Dataset, load_dataset
 from ftfy import fix_text
 from transformers import AutoTokenizer, T5TokenizerFast
+from quality_filter import QualityFilter
 
 
 def preprocess_function(examples):
@@ -36,23 +38,17 @@ ds = load_dataset(
 )
 
 ds_train = ds["train"]
-# ds_val = ds["validation"]
 
-# %% Filter by density
+# Filter by density
 df_abstractive = pd.DataFrame(ds_train)
 df_abstractive = df_abstractive[df_abstractive.density <= 1.5]
-print(len(df_abstractive))  # 287205
-# df_abstractive.to_csv("/home/sarakolind/DanSum/df_abstractive.csv")  # save csv
 
-# %% Clean by filter token article/summary length cutoffs
-# df_abstractive = pd.read_csv("/home/sarakolind/DanSum/df_abstractive.csv")  # load csv
-
-# %% Convert data format
+# Convert data format
 ds_abstractive = Dataset.from_pandas(df_abstractive)
 dd_abstractive = datasets.DatasetDict({"data": ds_abstractive})
 
-# # %% Load tokenizer
-tokenizer = AutoTokenizer.from_pretrained("google/mt5-small")  # choose tokenizer
+# Load tokenizer
+tokenizer = AutoTokenizer.from_pretrained("google/mt5-small") 
 
 t_sums = [fix_text(i) for i in ds_train["summary"]]
 t_texts = [fix_text(i) for i in ds_train["text"]]
@@ -62,65 +58,26 @@ ds_train = ds_train.remove_columns(["summary", "text"])
 ds_train = ds_train.rename_column("summary_fix", "summary")
 ds_train = ds_train.rename_column("text_fix", "text")
 
-# v_sums = [fix_text(i) for i in ds_val['summary']]
-# v_texts = [fix_text(i) for i in ds_val['text']]
-# ds_val = ds_val.add_column("summary_fix", v_sums)
-# ds_val = ds_val.add_column("text_fix", v_texts)
-# ds_val = ds_val.remove_columns(['summary', 'text'])
-# ds_val = ds_val.rename_column('summary_fix', 'summary')
-# ds_val = ds_val.rename_column('text_fix', 'text')
-
-# %% tokenize chosen data
+# tokenize
 tok_dd = ds_train.map(preprocess_function, batched=True)
-# tok_val = ds_val.map(preprocess_function, batched=True)
 tok_ds = tok_dd  # ["data"]  # turn back into dataset (not dict)
 
-# %% add features to ds with the TOKENISED lengths of ref summary and text/article
-tok_text_len = [len(text) for text in tok_dd["input_ids"]]  # input ids is the articles
+# %% add features to ds with the tokenised lengths of ref summary and text/article
+tok_text_len = [len(text) for text in tok_dd["input_ids"]] 
 tok_ds = tok_dd.add_column("tok_text_len", tok_text_len)
 tok_sum_len = [
     len(summary) for summary in tok_ds["labels"]
 ]  # labels is the ref summaries
 tok_ds = tok_ds.add_column("tok_sum_len", tok_sum_len)
 
-# tok_text_len = [len(text) for text in tok_val["input_ids"]]  # input ids is the articles
-# tok_val = tok_val.remove_columns("tok_text_len")
-# tok_dv = tok_val.add_column("tok_text_len", tok_text_len)
-# tok_sum_len = [
-#     len(summary) for summary in tok_val["labels"]
-# ]  # labels is the ref summaries
-# tok_dv = tok_dv.remove_columns("tok_sum_len")
-# tok_dv = tok_dv.add_column("tok_sum_len", tok_sum_len)
-
-# %% Make plot with summary length and text length to define cutoffs
-# histogram of summary token length
-# graph = seaborn.histplot(tok_ds["tok_sum_len"], color="red")
 min_sum_len = np.quantile(tok_ds["tok_sum_len"], 0.02)
 max_sum_len = np.quantile(tok_ds["tok_sum_len"], 0.98)
-# graph.axvline(min_sum_len)
-# graph.axvline(max_sum_len)
-# graph.set_xlabel("Number of tokens")
-# graph.set_title("Number of tokens in summaries")
-# graph.text(200, 1000, f"Lower cutoff: {round(min_sum_len, 1)}")
-# graph.text(200, 900, f"Upper cutoff: {round(max_sum_len, 1)}")
-# plt.savefig("/home/sarakolind/DanSum/sum_tokens_hist.png")
-# plt.show()
 
-# %% histogram of text token length
-# graph = seaborn.histplot(tok_ds["tok_text_len"], color="red")
 min_text_len = np.quantile(tok_ds["tok_text_len"], 0.02)
 max_text_len = np.quantile(tok_ds["tok_text_len"], 0.98)
-# graph.axvline(min_text_len)
-# graph.axvline(max_text_len)
-# graph.set_xlabel("Number of tokens")
-# graph.set_title("Number of tokens in article texts")
-# graph.text(30000, 800, f"Lower cutoff: {round(min_text_len, 1)}")
-# graph.text(30000, 700, f"Upper cutoff: {round(max_text_len, 1)}")
-# plt.savefig("/home/sarakolind/DanSum/text_tokens_hist.png")
-# plt.show()
 
 # %% Filtering based on cutoffs
-tok_df = pd.DataFrame(tok_ds)  # pandas dataframe version
+tok_df = pd.DataFrame(tok_ds) 
 
 # minimum summary length
 tok_df_clean = tok_df.loc[tok_df["tok_sum_len"] >= min_sum_len]
@@ -134,13 +91,7 @@ tok_df_clean = tok_df_clean.loc[tok_df_clean["tok_text_len"] <= max_text_len]
 # tok_df_clean = tok_df_clean.drop(columns='__index_level_0__')
 tok_ds_clean = Dataset.from_pandas(tok_df_clean)
 
-
-"""
-Quality filter is from the following github: https://github.com/centre-for-humanities-computing/danish-foundation-models/blob/b16765c065818f9d4b162d2b2ab9b3dae7d252ea/src/dfm/cleaning/quality_filter.py
-"""
-# %% Aplying quality filter
-# NB: Have the quality_filter.py script in your working directory
-from quality_filter import QualityFilter
+# apply filter
 
 qf = QualityFilter(
     min_stop_words=2,
@@ -200,13 +151,14 @@ qf_sum = QualityFilter(
     ],
     ignore_filters=filter_to_ignore,
 )
-# %%
+
 # Lists of texts
 texts = tok_ds_clean["text"]
 summaries = tok_ds_clean["summary"]
 filtered = qf.describe_filter(
     texts
-)  # giver jer hvilke som bliver filtreret fra og af hvilket filter
+) 
+
 filtered_sum = qf_sum.describe_filter(summaries)
 passed_quality = [None] * len(texts)
 filter = [None] * len(texts)  # empty list
@@ -222,19 +174,20 @@ for n, i in enumerate(texts):
     else:
         passed_quality[n] = False
         filter[n] = result
-    # same for summaries
+
     if result_sum == "passed filters":
         passed_quality_sum[n] = True
         filter_sum[n] = "nan"
     else:
         passed_quality_sum[n] = False
         filter_sum[n] = result_sum
+
 tok_ds_clean = tok_ds_clean.add_column("passed_quality", passed_quality)
 tok_ds_clean = tok_ds_clean.add_column("filter", filter)
 tok_ds_clean = tok_ds_clean.add_column("passed_quality_sum", passed_quality_sum)
 tok_ds_clean = tok_ds_clean.add_column("filter_sum", filter_sum)
 
-# %% Saving it
+# save
 tok_ds_clean.to_csv(
     "/data-big-projects/danish-summarization-danewsroom/tok_ds_clean_.csv"
 )
